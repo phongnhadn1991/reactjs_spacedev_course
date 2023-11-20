95ea7a6d543a9208221a7f979ffeebf2f2bb5c01
NgoanMc <ngoanmc@concrete-corp.com>
2023-11-20 16:08:23 +0700

    todoList

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,71 @@
+import { useState } from "react"
 import "./assets/tailwind.css"
+import ToDoList from "./components/ToDoList"
 
 function App() {
+  const defaultToDoList = [
+    {
+      id: 1,
+      name: 'Công Việc A',
+      isCompleted: false
+    },
+    {
+      id: 2,
+      name: 'Công Việc B',
+      isCompleted: false
+    },
+    {
+      id: 3,
+      name: 'Công Việc C',
+      isCompleted: true
+    },
+  ]
+  const [toDoList, setToDoList] = useState(defaultToDoList)
+
+  const onAdd = (name) => {
+    const task = {
+      id: Date.now(),
+      name,
+      isCompleted: false
+    }
+    setToDoList([...toDoList, task])
+  }
+
+  const onCompleted = (id) => {
+    let task = toDoList.find(e => e.id === id)
+    if (task) {
+      task.isCompleted = true
+      setToDoList([...toDoList])
+    }
+  }
+
+  const onDoing = (id) => {
+    let task = toDoList.find(e => e.id === id)
+    if (task) {
+      task.isCompleted = false
+      setToDoList([...toDoList])
+    }
+  }
+
+  const handleStatus = (id) => {
+    let task = toDoList.find(e => e.id === id)
+    console.log(toDoList)
+    if (task) {
+      if (task.isCompleted == false) {
+        onCompleted(id)
+      } else {
+        onDoing(id)
+      }
+    }
+  }
 
   return (
     <>
-
+      <ToDoList
+        toDoList={toDoList}
+        onAdd={onAdd}
+        handleStatus={handleStatus}
+      ></ToDoList>
     </>
   )
 }
---
+++ src/components/ToDoList/index.jsx
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const ToDoListStyle = styled.div`
+  .items {
+    display: flex;
+    flex-flow: column;
+    gap: 15px;
+  }
+`;
+
+export default function ToDoList({ toDoList, onAdd, handleStatus }) {
+  const listDoing = toDoList.filter((e) => !e.isCompleted);
+  const listDone = toDoList.filter((e) => e.isCompleted);
+
+  const [value, setValue] = useState("");
+  const _onAdd = () => {
+    onAdd(value.trim());
+    setValue("");
+  };
+
+  return (
+    <ToDoListStyle className="px-4 py-4">
+      <form className="w-full mb-5">
+        <div className="flex items-center border-b border-teal-500 py-2">
+          <input
+            className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+            type="text"
+            placeholder="Enter todo"
+            onChange={(e) => {
+              setValue(e.target.value);
+            }}
+            value={value}
+          />
+          <button
+            className="flex-shrink-0 bg-teal-500 hover:bg-teal-700  text-white py-3 px-4 rounded hover:border-teal-700 text-sm font-semibold disabled:bg-gray-300 disabled:cursor-not-allowed "
+            type="button"
+            disabled={!value.trim()}
+            onClick={_onAdd}
+          >
+            Add
+          </button>
+        </div>
+      </form>
+      <div className="flex gap-6">
+        <div className="w-full ml-auto h-12">
+          <h4 className="text-xl font-semibold mb-4">Doing</h4>
+          <div className="items">
+            {listDoing.map((todo) => (
+              <ToDoItem key={todo.id} handleStatus={handleStatus} {...todo} />
+            ))}
+          </div>
+        </div>
+        <div className="w-full ml-auto h-12">
+          <h4 className="text-xl font-semibold mb-4">Done</h4>
+          <div className="items">
+            {listDone.map((todo) => (
+              <ToDoItem key={todo.id} handleStatus={handleStatus} {...todo} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </ToDoListStyle>
+  );
+}
+
+const ToDoItemStyle = styled.div`
+  display: flex;
+  gap: 20px;
+  justify-content: space-between;
+  padding: 10px 15px;
+  background: #f3f3f3;
+  &.isCompleted {
+    background: #dcdcdc;
+    .name {
+      text-decoration: line-through;
+    }
+  }
+`;
+
+export const ToDoItem = (props) => {
+  const { id, name, isCompleted, handleStatus } = props;
+  return (
+    <ToDoItemStyle className={isCompleted ? "isCompleted" : ""}>
+      <div className="name">{name}</div>
+      <label className="relative inline-flex items-center cursor-pointer">
+        <input
+          type="checkbox"
+          defaultValue
+          className="sr-only peer"
+          defaultChecked={isCompleted}
+          onClick={() => handleStatus(id)}
+          // onChange={(e) => console.log(e)}
+        />
+        <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-600" />
+      </label>
+    </ToDoItemStyle>
+  );
+};

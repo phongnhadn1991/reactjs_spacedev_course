d92750767e1f21ea82225d03bf5123be0d189fbe
NgoanMc <ngoanmc@concrete-corp.com>
2023-11-20 11:23:02 +0700

    reset app

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
-import Box from "./components/Box"
 import "./assets/tailwind.css"
 
 function App() {
-  const atrributeBox = {
-    background: 'green'
-  }
 
   return (
     <>
-      <Box {...atrributeBox}>
-        Green
-      </Box>
-      <Box background="red">
-        Red
-      </Box>
+
     </>
   )
 }

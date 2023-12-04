3da5f2a41aadb5b0cc9533c24b51cdd906f326e2
NgoanMc <ngoanmc@concrete-corp.com>
2023-12-04 16:37:17 +0700

    page course

--- src/pages/course/index.jsx
+++ src/pages/course/index.jsx
@@ -1,118 +1,87 @@
 import React, { useEffect, useState } from 'react'
 import { courseService } from '../../services/course.service'
-import { Avatar, List, Radio, Space } from 'antd';
+import { Col, Pagination, Row } from 'antd';
+import styled from 'styled-components'
+import { Link } from 'react-router-dom';
 
 export default function Course() {
   const [loading, setLoading] = useState(true)
+  const itemsPerPage = 9
   const [cousrses, setCourses] = useState([])
-
-  const data = [
-    {
-      title: 'Ant Design Title 1',
-    },
-    {
-      title: 'Ant Design Title 2',
-    },
-    {
-      title: 'Ant Design Title 3',
-    },
-    {
-      title: 'Ant Design Title 4',
-    },
-  ];
+  const [paginate, setPaginate] = useState(null)
+  const [currentPage, setCurrentPage] = useState(1)
 
   const fetchData = async () => {
     setLoading(true)
     const res = await courseService.getCourse()
     if (res) {
       setCourses(res?.data?.data)
+      setPaginate(res?.data?.paginate)
     }
     setLoading(false)
   }
-
   useEffect(() => {
     fetchData();
   }, [])
 
+  const onChangePage = (page) => {
+    console.log(history)
+    setCurrentPage(page)
+  }
+
+  const totalPage = Math.ceil(paginate?.count / itemsPerPage);
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = Math.min(startIndex + itemsPerPage - 1, paginate?.count - 1);
+  const itemsForCurrentPage = cousrses.slice(startIndex, endIndex + 1);
 
   return (
     <div className='l-course'>
-      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                Title
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Price
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Video
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Time
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              loading ? <Spiner /> :
-                cousrses && cousrses.map(item => <CourseItem key={item._id} item={item} />)
-            }
-          </tbody>
-        </table>
-      </div>
-      <List
-        pagination
-        dataSource={cousrses}
-        renderItem={(item, index) => (
-          <List.Item>
-            <List.Item.Meta
-              avatar={
-                <Avatar src={`https://xsgames.co/randomusers/avatar.php?g=pixel&key=${index}`} />
-              }
-              title={<a href="https://ant.design">{item.title} - {item.money}</a>}
-              description={item.opening_time}
-            />
-          </List.Item>
-        )}
-      />
+      <Row gutter={[30, 30]}>
+        {itemsForCurrentPage.map(item => <Col key={item._id} span={8}><ItemCourse item={item} /></Col>)}
+      </Row>
+      <Pagination
+        current={currentPage}
+        total={paginate?.count}
+        pageSize={itemsPerPage}
+        onChange={(currentPage) => onChangePage(currentPage)}
+        style={{ margin: '60px 0 30px', display: 'flex', justifyContent: 'center' }} />
     </div>
   )
 }
 
-export const CourseItem = ({ item }) => {
-  return (
-    <tr className="bg-white border-b hover:bg-gray-50">
-      <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-        {item.title}
-      </th>
-      <td className="px-6 py-4">
-        {item.money}
-      </td>
-      <td className="px-6 py-4">
-        {item.count_video}
-      </td>
-      <td className="px-6 py-4">
-        {item.opening_time}
-      </td>
-    </tr>
-  )
-}
 
-export const Spiner = () => {
+export const ImgStyle = styled.div`
+width: 100%;
+height: 220px;
+margin-bottom: 15px;
+background: #fafafa;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`
+export const DesStyle = styled.div`
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;  
+  overflow: hidden;
+  color: #000;
+`
+export const ItemCourse = ({ item }) => {
   return (
-    <tr>
-      <td colSpan={4} className='text-center py-4 px-4'>
-        <div role="status">
-          <svg aria-hidden="true" className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
-            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
-          </svg>
-          <span className="sr-only">Loading...</span>
-        </div>
-      </td>
-    </tr>
+    <Link to={`/course/${item.id}`} className='itemCourse'>
+      <div className='itemCourse_thumb' >
+        <ImgStyle>
+          {item.thumbnailUrl && <img src={item.thumbnailUrl} alt={item.title} />}
+        </ImgStyle>
+      </div>
+      <div className='itemCourse_body'>
+        <h4>{item.title}</h4>
+        <DesStyle>
+          {item.long_description}
+        </DesStyle>
+      </div>
+    </Link>
   )
 }
